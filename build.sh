f0de5fdfae0dbefd5ad7a5b2574f4be1d59ff6bb
Chris Malton <chrism@cmalton.me.uk>
2013-02-27 13:45:05 +0000

    Make the build script "fix" puppet so it works on Debian

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-apt-get -y install git ruby-ldap
+apt-get -y install git ruby-ldap puppet
+# Because debian is insane
+sed -ie 's|#!.*/usr/bin/ruby1.8|#!/usr/bin/ruby|' /usr/bin/puppet
 rm -rf /etc/puppet
-git clone git://studentrobotics.org/server/puppet.git /etc/puppet
+git clone git://studentrobotics.org/network/router/puppet.git /etc/puppet
 cd /etc/puppet && git submodule init
 cd /etc/puppet && git submodule update
-puppet apply /etc/puppet/manifests/sr-router-dev.pp
+puppet apply /etc/puppet/manifests/sr-dev-router.pp
 
 #wget --no-check-certificate https://localhost -O /dev/null
 #if [ $? ]
