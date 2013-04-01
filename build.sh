20e83ba570cefad0807fda5f85455cd6687b5a3f
Chris Malton <chrism@cmalton.me.uk>
2013-04-01 09:22:25 +0100

    Fix the build script to work properly
    Missing apt-get update/apt-get upgrade would cause some issues.

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-apt-get -y install git ruby-ldap puppet
+apt-get -y update                           # Package updates first please
+apt-get -y upgrade                          # Do all upgrades
+apt-get -y dist-upgrade                     # Do all upgrades
+apt-get -y install git ruby-ldap puppet     # Now install the extras we need
+
 # Because debian is insane
 sed -ie 's|#!.*/usr/bin/ruby1.8|#!/usr/bin/ruby|' /usr/bin/puppet
+
+# Dump the puppet config in
 rm -rf /etc/puppet
 git clone git://studentrobotics.org/network/router/puppet.git /etc/puppet
 cd /etc/puppet && git submodule init
 cd /etc/puppet && git submodule update
+# Apply puppet
 puppet apply /etc/puppet/manifests/sr-dev-router.pp
 
-#wget --no-check-certificate https://localhost -O /dev/null
-#if [ $? ]
-#then
-    echo "Server configured successfully!"
-#else
-#    echo "Server configuration went wrong :("
-#fi
+echo "Server configured successfully!"
